e683d2403e2dd8c41258e80051d20aadba1f3637
Michal Kucera <michal.kucera04@gmail.com>
2022-04-20 11:34:32 +0200

    add anchor link

--- views/index.jsx
+++ views/index.jsx
@@ -1,5 +1,6 @@
 const React = require("react");
 const { Fragment } = require("react");
+var sizeOf = require("image-size");
 
 const textColors = {
    default: "rgb(55, 53, 47)",
@@ -102,6 +103,7 @@ const renderBlock = ({ block, params, level = 0 }) => {
    const webflow = params.webflow == "true";
    const improvedLists = params.improvedLists == "true";
    const headingIds = params.headingIds == "true";
+   const headingAnchors = params.headingAnchors == "true";
 
    const { type, id } = block;
    const value = block[type];
@@ -112,13 +114,35 @@ const renderBlock = ({ block, params, level = 0 }) => {
       orderedListCount[level] = 1;
    if (level == 0) orderedListCount[1] = 1;
 
-   // Handle in-page heading links
+   // Handle in-page heading links, heading anchors
+   const headingId = id.replace(/-/g, "");
    const headingProps = type.includes("heading")
       ? {
            style: colorOrBg,
-           id: headingIds ? id.replace(/-/g, "") : undefined,
+           id: headingId,
         }
       : undefined;
+   const linkSymbol = (
+      <svg
+         class="octicon octicon-link"
+         viewBox="0 0 16 16"
+         color="#c9d1d9"
+         version="1.1"
+         width="16"
+         height="16"
+         aria-hidden="true"
+      >
+         <path
+            fill-rule="evenodd"
+            d="M7.775 3.275a.75.75 0 001.06 1.06l1.25-1.25a2 2 0 112.83 2.83l-2.5 2.5a2 2 0 01-2.83 0 .75.75 0 00-1.06 1.06 3.5 3.5 0 004.95 0l2.5-2.5a3.5 3.5 0 00-4.95-4.95l-1.25 1.25zm-4.69 9.64a2 2 0 010-2.83l2.5-2.5a2 2 0 012.83 0 .75.75 0 001.06-1.06 3.5 3.5 0 00-4.95 0l-2.5 2.5a3.5 3.5 0 004.95 4.95l1.25-1.25a.75.75 0 00-1.06-1.06l-1.25 1.25a2 2 0 01-2.83 0z"
+         ></path>
+      </svg>
+   );
+   const headingAnchor = headingAnchors ? (
+      <a href={headingId} className="heading-anchor" aria-hidden="true">
+         {linkSymbol}
+      </a>
+   ) : undefined;
 
    switch (type) {
       case "paragraph":
@@ -130,18 +154,21 @@ const renderBlock = ({ block, params, level = 0 }) => {
       case "heading_1":
          return (
             <h1 {...headingProps}>
+               {headingAnchor}
                <Text text={value.rich_text} />
             </h1>
          );
       case "heading_2":
          return (
             <h2 {...headingProps}>
+               {headingAnchor}
                <Text text={value.rich_text} />
             </h2>
          );
       case "heading_3":
          return (
             <h3 {...headingProps}>
+               {headingAnchor}
                <Text text={value.rich_text} />
             </h3>
          );
@@ -266,6 +293,9 @@ const renderBlock = ({ block, params, level = 0 }) => {
          const src =
             value.type === "external" ? value.external.url : value.file.url;
          const plainCaption = value.caption ? value.caption[0]?.plain_text : "";
+
+         var dimensions = sizeOf(src);
+         console.log(dimensions);
 
          // Makes image full width by default
          // if caption is "center", image will be centered and caption hidden
