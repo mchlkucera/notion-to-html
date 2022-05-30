1a79dc0e2264b2038f76015ff72fa20d541515c8
Michal Kucera <michal.kucera04@gmail.com>
2022-05-30 17:42:02 +0200

    delete test file

--- test.js
+++
@@ -1,4 +0,0 @@
-const url = "https://www.youtube.com/watch?v=K8bhKeV4nrQ&blablabla";
-const end = url.lastIndexOf("&");
-const x = url.substring(url.indexOf("v=") + 2, end > 0 ? end : url.length);
-console.log(x);
